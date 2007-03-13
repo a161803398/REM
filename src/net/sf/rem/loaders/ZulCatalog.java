bb939fcf77ad20d98e05f0e81b04a43a6bda82fd
magic003 <magic003@999a35c4-ac9e-44b2-8034-66d316cfc8a1>
2007-03-13 14:17:40 +0000

    Add ZUL Catalog, so the source editor can provide DTD based code completion.

---
+++ src/net/sf/rem/loaders/ZulCatalog.java
@@ -0,0 +1,93 @@
+/*
+ * ZulCatalog.java
+ *
+ * Created on February 27, 2007, 9:20 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package net.sf.rem.loaders;
+
+import java.awt.Image;
+import java.beans.PropertyChangeListener;
+import java.io.IOException;
+import java.util.Iterator;
+import org.netbeans.modules.xml.catalog.spi.CatalogDescriptor;
+import org.netbeans.modules.xml.catalog.spi.CatalogListener;
+import org.netbeans.modules.xml.catalog.spi.CatalogReader;
+import org.openide.util.Utilities;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author magic
+ */
+public class ZulCatalog implements CatalogReader, CatalogDescriptor, org.xml.sax.EntityResolver{
+    
+    public static final String JAVAEE_NS = "http://java.sun.com/xml/ns/javaee";  // NOI18N
+    private static final String ZUL_2_0_XSD="zul.xsd"; // NOI18N
+    private static final String ZUL_2_0=JAVAEE_NS+"/"+ZUL_2_0_XSD; // NOI18N
+    public static final String ZUL_ID_2_0="SCHEMA:"+ZUL_2_0; // NOI18N
+    private static final String URL_ZUL_2_0="nbres:/net/sf/rem/resources/zul.xsd"; // NOI18N
+    private static final String URL_ZUL_2_0_DTD = "nbres:/net/sf/rem/resources/zul.dtd"; // NOI18N
+    
+    /** Creates a new instance of ZulCatalog */
+    public ZulCatalog() {
+    }
+
+    public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
+        if(ZUL_ID_2_0.equals(publicId)){
+            return new org.xml.sax.InputSource(URL_ZUL_2_0_DTD);
+            //return new org.xml.sax.InputSource("nbres:/net/sf/rem/resources/zul.dtd");
+        }
+        return null;
+    }
+
+    public Iterator getPublicIDs() {
+        java.util.List list = new java.util.ArrayList();
+        list.add(ZUL_ID_2_0);
+        return list.listIterator();
+    }
+
+    public void refresh() {
+    }
+
+    public String getSystemID(String string) {
+        return URL_ZUL_2_0_DTD;
+    }
+
+    public String resolveURI(String string) {
+        return null;
+    }
+
+    public String resolvePublic(String string) {
+        return null;
+    }
+
+    public void addCatalogListener(CatalogListener catalogListener) {
+    }
+
+    public void removeCatalogListener(CatalogListener catalogListener) {
+    }
+
+    public Image getIcon(int type) {
+        return Utilities.loadImage("net/sf/rem/resources/zulCatalog.png");
+    }
+
+    public String getDisplayName() {
+        return "ZUL Catalog";
+    }
+
+    public String getShortDescription() {
+        return "XML Catalog for ZUL 2.0";
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener propertyChangeListener) {
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener propertyChangeListener) {
+    }
+    
+}
---
+++ src/net/sf/rem/loaders/ZulGrammarQueryManager.java
@@ -0,0 +1,63 @@
+/*
+ * ZulGrammarQueryManager.java
+ *
+ * Created on February 27, 2007, 10:21 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package net.sf.rem.loaders;
+
+import java.beans.FeatureDescriptor;
+import java.util.Enumeration;
+import org.netbeans.api.xml.services.UserCatalog;
+import org.netbeans.modules.xml.api.model.DTDUtil;
+import org.netbeans.modules.xml.api.model.GrammarEnvironment;
+import org.netbeans.modules.xml.api.model.GrammarQuery;
+import org.openide.util.Enumerations;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author magic
+ */
+public class ZulGrammarQueryManager extends org.netbeans.modules.xml.api.model.GrammarQueryManager {
+    
+    /** Creates a new instance of ZulGrammarQueryManager */
+    public ZulGrammarQueryManager() {
+    }
+
+    public Enumeration enabled(GrammarEnvironment grammarEnvironment) {
+        return Enumerations.empty();
+    }
+
+    public GrammarQuery getGrammar(GrammarEnvironment grammarEnvironment) {
+        UserCatalog catalog = UserCatalog.getDefault();
+        if (catalog != null) {
+            EntityResolver resolver = catalog.getEntityResolver();
+            if (resolver != null) {
+                try {
+                    String schema = ZulCatalog.ZUL_ID_2_0;
+                    InputSource inputSource = resolver.resolveEntity(schema, null);
+                    if (inputSource!=null) {
+                        // parse DTD file
+                        return DTDUtil.parseDTD(true, inputSource);
+                    }
+                } catch(SAXException e) {
+                    e.printStackTrace(System.out);
+                } catch(java.io.IOException e) {
+                    e.printStackTrace(System.out);
+                }
+            }
+        }
+        return null;
+    }
+
+    public FeatureDescriptor getDescriptor() {
+        return new java.beans.FeatureDescriptor();
+    }
+    
+}
